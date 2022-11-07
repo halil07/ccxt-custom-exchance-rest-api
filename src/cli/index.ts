83775a2fa3554964a13aec62a6081c9fec76b2bb
Halil Ceyhan <halilceyhan@yandex.com>
2022-11-08 01:10:04 +0300

    fixes

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -6,14 +6,14 @@ const exchanges = ccxt.exchanges
 fs.rmSync(path.join(__dirname, "./types"), { recursive: true, force: true });
 fs.mkdirSync(path.join(__dirname, "./types"));
 (async () => {
-    const writeIndexStream = fs.createWriteStream(path.resolve(__dirname, `./import.js`));
+    const writeIndexStream = fs.createWriteStream(path.resolve(__dirname, `../src/index.ts`));
     exchanges.forEach(async (exchange, index)=>{
-        const writeStream = fs.createWriteStream(path.resolve(__dirname, `./types/${exchange}.d.ts`));
+        const writeStream = fs.createWriteStream(path.resolve(__dirname, `./types/${exchange}.ts`));
         const exchangeInstance = new ccxt[exchange]();
         const restApi = await defineRestApi(exchangeInstance.api, "request", []);
-        writeStream.write(`import {${exchange} as Type} from 'ccxt';\nexport class ${exchange}RestApiType extends Type {\n\t${Object.keys(restApi).map(method => `${method}: (params:{}, context?:{}) => Promise<any>;`).join('\n\t')}\n}`);
+        writeStream.write(`import {${exchange} as Type} from 'ccxt';\nexport interface ${exchange}RestApiType extends Type {\n\t${Object.keys(restApi).map(method => `${method}: (params:{}, context?:{}) => Promise<any>;`).join('\n\t')}\n}`);
         writeStream.end();
-        writeIndexStream.write(`export {${exchange}RestApiType} from './types/${exchange}';\n`)
+        writeIndexStream.write(`export type {${exchange}RestApiType} from './types/${exchange}';\n`)
         console.log("exchange", exchange, index+1, ccxt.exchanges.length, Object.keys(restApi).length)
     })
     await new Promise(r => setTimeout(r, 2000));
