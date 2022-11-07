14179a74307b3b36586b45771374af48fa5a4060
Halil Ceyhan <halilceyhan@yandex.com>
2022-11-08 00:33:45 +0300

    fixes

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -5,11 +5,18 @@ import path from "node:path";
 const exchanges = ccxt.exchanges
 fs.rmSync(path.join(__dirname, "./types"), { recursive: true, force: true });
 fs.mkdirSync(path.join(__dirname, "./types"));
-exchanges.forEach(async (exchange, index)=>{
-    const writeStream = fs.createWriteStream(path.resolve(__dirname, `./types/${exchange}.d.ts`));
-    const exchangeInstance = new ccxt[exchange]();
-    const restApi = await defineRestApi(exchangeInstance.api, "request", []);
-    writeStream.write(`import {${exchange} as Type} from 'ccxt';\nexport class ${exchange}RestApiType extends Type {\n\t${Object.keys(restApi).map(method => `${method}: (params:{}, context?:{}) => Promise<any>;`).join('\n\t')}\n}`);
-    writeStream.end();
-    console.log("exchange", exchange, index+1, ccxt.exchanges.length, Object.keys(restApi).length)
-})
+(async () => {
+    const writeIndexStream = fs.createWriteStream(path.resolve(__dirname, `./import.js`));
+    exchanges.forEach(async (exchange, index)=>{
+        const writeStream = fs.createWriteStream(path.resolve(__dirname, `./types/${exchange}.d.ts`));
+        const exchangeInstance = new ccxt[exchange]();
+        const restApi = await defineRestApi(exchangeInstance.api, "request", []);
+        writeStream.write(`import {${exchange} as Type} from 'ccxt';\nexport class ${exchange}RestApiType extends Type {\n\t${Object.keys(restApi).map(method => `${method}: (params:{}, context?:{}) => Promise<any>;`).join('\n\t')}\n}`);
+        writeStream.end();
+        writeIndexStream.write(`export {${exchange}RestApiType} from './types/${exchange}';\n`)
+        console.log("exchange", exchange, index+1, ccxt.exchanges.length, Object.keys(restApi).length)
+    })
+    await new Promise(r => setTimeout(r, 2000));
+    writeIndexStream.end();
+})()
+
